337c913897e3ae294adc0e2965c7b1d1a4ec0431
Pedro <solisgpedro@gmail.com>
2026-02-13 15:36:51 +0100

    NO-JIRA | Expand Colors and SFSymbols

--- Sources/KalistenColors/Colors.swift
+++ Sources/KalistenColors/Colors.swift
@@ -1,14 +1,14 @@
 //
-//  Copyright © Kalisten 2024
+//  Copyright © Kalisten 2024-2026
 //
 
 import SwiftUI
 
 public enum Colors {
     public enum Primary {
-        private static let light = Color(Asset.lightTangarine.color)
+        public static let light = Color(Asset.lightTangarine.color)
         public static let dark = Color(Asset.darkTangarine.color)
-        private static let darker = Color(Asset.darkerTangarine.color)
+        public static let darker = Color(Asset.darkerTangarine.color)
 
         public enum Mid {
             private static let color = Color(Asset.midTangarine.color)
@@ -21,8 +21,8 @@ public enum Colors {
     }
     public enum Secondary {
         public static let light = Color(Asset.lightGrey.color)
-        private static let dark = Color(Asset.darkGrey.color)
-        private static let darker = Color(Asset.darkerGrey.color)
+        public static let dark = Color(Asset.darkGrey.color)
+        public static let darker = Color(Asset.darkerGrey.color)
 
         public enum Lighter {
             private static let color = Color(Asset.lighterGrey.color)
@@ -45,6 +45,32 @@ public enum Colors {
         public static let mid = Color(Asset.midPebble.color)
         public static let dark = Color(Asset.darkPebble.color)
         public static let darker = Color(Asset.darkerPebble.color)
+
+        public enum Light {
+            private static let color = Color(Asset.lightPebble.color)
+            public static let opaque = color.opacity(1.0)
+            public static let semiOpaque = color.opacity(0.75)
+            public static let semiTranslucent = color.opacity(0.5)
+        }
+        public enum Mid {
+            private static let color = Color(Asset.midPebble.color)
+            public static let opaque = color.opacity(1.0)
+            public static let semiOpaque = color.opacity(0.75)
+            public static let semiTranslucent = color.opacity(0.5)
+            public static let translucent = color.opacity(0.25)
+        }
+        public enum Dark {
+            private static let color = Color(Asset.darkPebble.color)
+            public static let opaque = color.opacity(1.0)
+            public static let semiTranslucent = color.opacity(0.5)
+            public static let translucent = color.opacity(0.25)
+        }
+        public enum Darker {
+            private static let color = Color(Asset.darkerPebble.color)
+            public static let opaque = color.opacity(1.0)
+            public static let semiTranslucent = color.opacity(0.5)
+            public static let translucent = color.opacity(0.2)
+        }
     }
     public enum System {
         public static let success = Color(Asset.lime.color)
